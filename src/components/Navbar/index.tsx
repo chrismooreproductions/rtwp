eb2441cd188f8acf6b39e2809e4a488c2b9ddc53
Christopher Moore <chrismooreproductions@gmail.com>
2018-09-23 22:52:07 +0100

    getting there with material themes

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,17 +1,73 @@
 import * as React from 'react';
-import { NavbarWrapper, StyledLink } from './styles';
+import { createStyles, withStyles } from '@material-ui/core/styles';
+import { WithStyles } from '@material-ui/core';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import { mailFolderListItems, otherMailFolderListItems } from './tileData';
 
-const NavBar = () => (
-  <NavbarWrapper>
-    <StyledLink 
-        to="/another-page">
-        Other Page
-    </StyledLink>
-    <StyledLink
-        to="/app">
-        App
-    </StyledLink>
-  </NavbarWrapper>
-)
+const drawerWidth = 240;
 
-export default NavBar
+const styles = (theme: Theme) => createStyles ({
+  root: {
+    flexGrow: 1,
+    height: 440,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawerPaper: {
+    position: 'relative',
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+    minWidth: 0, // So the Typography noWrap works
+  },
+  toolbar: theme.mixins.toolbar,
+});
+
+interface Props extends WithStyles<typeof styles> {}
+
+function Navbar(props: Props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="absolute" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="title" color="inherit" noWrap>
+            Clipped drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.toolbar} />
+        <List>{mailFolderListItems}</List>
+        <Divider />
+        <List>{otherMailFolderListItems}</List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>
+      </main>
+    </div>
+  );
+}
+
+export default withStyles(styles)(Navbar);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,22 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import TodosApp from '../components/Todos';
 import AnotherPage from '../components/AnotherPage';
 import Navbar from '../components/Navbar';
+import { createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import green from '@material-ui/core/colors/green';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: green,
+    type: "dark",
+  },
+});
 
 const routes = (
   <Router>
     <div>
-      <Navbar />
+      <Navbar theme={theme}/>
       <Switch>
         <Route path="/another-page" component={AnotherPage} />
         <Route path="/app" component={TodosApp} />
