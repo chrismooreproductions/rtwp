981788e641dae37b8d3125b7aea39fb86be23660
Christopher Moore <chrismooreproductions@gmail.com>
2018-09-26 22:04:33 +0100

    Created formatters in utilities
    Added convertStringToCamelCase method in formatters
    
    Adding switch to generate each item in list with custom icon

--- src/components/Navbar/buttons.tsx
+++ src/components/Navbar/buttons.tsx
@@ -4,11 +4,12 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import List from '@material-ui/core/List';
-import { Link } from 'react-router-dom';
+import { convertStringToCamelCase } from '../../utilities/formatters';
 
 interface ButtonListProp {
   id: number;
   role: string;
+  icon: string;
 }
 
 interface ButtonListProps {
@@ -16,20 +17,56 @@ interface ButtonListProps {
 }
 
 export const buttonDefinition: ButtonListProp[] = [
-  { id: 0, role: 'Transmission' },
-  { id: 1, role: 'Transfer Box' },
-  { id: 2, role: 'Propshafts' },
-  { id: 3, role: 'Rear Drive Unit' },
-  { id: 4, role: 'Front Drive Unit' }
+  { id: 0, role: 'Transmission', icon: 'inboxIcon' },
+  { id: 1, role: 'Transfer Box', icon: 'two' },
+  { id: 2, role: 'Propshafts', icon: 'three' },
+  { id: 3, role: 'Rear Drive Unit', icon: 'four' },
+  { id: 4, role: 'Front Drive Unit', icon: 'five' }
 ];
+
+interface NavbarIcons{
+  icon: string;
+}
+
+const NavbarIcon = (props: NavbarIcons) => {
+  const icon = props.icon;
+  switch(icon) {
+    case('inboxIcon'): 
+      return (
+        <ListItemIcon>
+          <InboxIcon />
+        </ListItemIcon>
+      )
+      break
+    case('two'):
+      return (
+        <ListItemIcon>
+          <InboxIcon />
+        </ListItemIcon>
+      )
+      break
+    default: 
+      return null
+  }
+}
+
+
+function handleClick() {
+  console.log('handling click');
+}
 
 export function ButtonList(props: ButtonListProps) {
   const navbarButtons = props.navbarButtons;
-  const listNavbarButtons = navbarButtons.map( button => 
-    <ListItem button key={button.id}>
-      <ListItemIcon>
-        <InboxIcon />
-      </ListItemIcon>
+  const listNavbarButtons = navbarButtons.map(button => 
+    <ListItem 
+      button 
+      key={button.id}
+      // hack to extend ListItem props
+      {...{ link: convertStringToCamelCase(button.role), burger: "sauce"  } as any}
+      onClick={() => handleClick()}>
+      <NavbarIcon 
+        icon={button.icon}
+      />
       <ListItemText primary={button.role} />
     </ListItem>
   )
---
+++ src/utilities/formatters.ts
@@ -0,0 +1,16 @@
+/*
+ * Utility to convert string
+ * to camelCase
+*/
+
+interface StringToCamelCase {
+  title?: string;
+  replace: any;
+}
+
+export function convertStringToCamelCase(string: StringToCamelCase) {
+  return string.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match: string, index: number) {
+    if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+    return index == 0 ? match.toLowerCase() : match.toUpperCase();
+  });
+}
